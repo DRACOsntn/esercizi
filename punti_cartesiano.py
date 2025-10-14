b2be19204261fc754cd7f130638108a8f183d493
DRACOsntn <jacopoaldo.misischi@campusdavinci.edu.it>
2025-10-14 11:35:05 +0200

    Add files via upload

--- punti_cartesiano.py
+++ punti_cartesiano.py
@@ -1,21 +1,43 @@
 #dato un insieme di punti  su un piano cartesiano,
 #random compresinell'intervallo
-# 0,10. calcolare il punto  con ascissa massima eil punti con ordinata minima
+# 0,10. calcolare il punto  con ascissa massima e il punti con ordinata minima
+
 import random
-contatore=0
+
 x=[]
 y=[]
 for i in range(0,20):
     x.append(random.randint(0,10))
     y.append(random.randint(0,10))
     
-    
+#ascissa massima
+#scorrere la lista delle x, trovare il massimo e salvare l'indice
+#visualizzare poi con un print il numero
+massimo=x[0]
+indice_max=0
+#minimo=y[0]
+#inidce_min=0
+
+for i in range (0,20):
+    if x[i]>massimo:
+        massimo=x[i]
+        indice_max=i
+print(massimo,y[indice_max])
+        
+for i in range(0,20):
+    if y[i]<minimo:
+        minimo=y[i]
+        indice_min=i
+        #print(minimo,x[indice_min])
+
+
+
 punti_cartesiano=[]
 for i in range (0,20):
     punto=(random.randint(0,10),random.randint(0,10))
     punti_cartesiano.append(punto)
             
 #come accedere alla x del primo punto
-print(punti_cartesiano[0][0])
+#print(punti_cartesiano[0][0])
 #come accedere alla y del primo punto
-print(punti_cartesiano[0][1])
+#print(punti_cartesiano[0][1])
